bcef520f8327730b79e6e2e9fee19d2e7e1137e8
bushra-mulla <bushra.1416@hotmail.com>
2021-01-04 20:29:52 +0300

    step1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="film-library">
+      <div className="film-list">
+        <hi className="section-title">Films</hi>
+      </div>
+      <div className="fil-details">
+        <h1 className="section-title">DETEILS</h1>
+      </div>
     </div>
   );
 }
