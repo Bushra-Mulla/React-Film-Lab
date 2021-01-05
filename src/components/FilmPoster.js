a6a4533a94eca259977da9e82a0c737073060ff2
bushra-mulla <bushra.1416@hotmail.com>
2021-01-05 09:39:06 +0300

    PART 1 Complete

---
+++ src/components/FilmPoster.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class FilmPoster extends Component {
+  render() {
+    const posterUrl = "https://image.tmdb.org/t/p/w780/";
+
+    return (
+      <div>
+        <img
+          className="poster"
+          src={posterUrl + this.props.img}
+          alt="no image"
+        />
+      </div>
+    );
+  }
+}
--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -4,11 +4,9 @@ import FilmPoster from "./FilmPoster";
 export default class FilmRow extends Component {
   render() {
     const filmRows = this.props.films.map((elem) => {
-      //   return <div >{row}</div>;
-      const posterUrl = "https://image.tmdb.org/t/p/w780/";
       return (
         <div className="film-row" key={elem.id}>
-          <img src={posterUrl + elem.poster_path} alt="" />
+          <FilmPoster img={elem.poster_path} />
           <div className="film-summary">
             <h1>{elem.title}</h1>
             <p>{new Date(elem.release_date).getFullYear()}</p>
@@ -16,7 +14,6 @@ export default class FilmRow extends Component {
         </div>
       );
     });
-    // console.log(filmRows); //
     return <div className="film-row">{filmRows}</div>;
   }
 }
