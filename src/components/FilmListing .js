2a3d57f9929bb92d3fe64f97f06d0f5c54e9e7f9
bushra-mulla <bushra.1416@hotmail.com>
2021-01-06 08:41:07 +0300

    part2_complete!

--- src/components/FilmListing .js
+++ src/components/FilmListing .js
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import FilmRow from "./FilmRow";
 
 export default class FilmListing extends Component {
-  handleFilterClick = (e, filter) => {
-    return console.log(`filter to`, filter);
+  constructor(props) {
+    super();
+    this.state = {
+      filter: "All",
+    };
+  }
+  handleFilterClick = (filter) => {
+    this.setState({ filter });
   };
   render() {
+    const isAllActive = this.state.filter === "All" ? "is-active" : "";
+    const isFavesActive = this.state.filter === "faves" ? "is-active" : "";
+
     return (
       <div className="film-list">
         <h1 className="section-title">Films {}</h1>
         <div className="film-list-filters">
           <div
-            className="film-list-filter"
+            className={`film-list-filter ${isAllActive}`}
             onClick={() => this.handleFilterClick("ALL")}
           >
             ALL
             <span>{this.props.films.length}</span>
           </div>
           <div
-            className="film-list-filter"
-            onClick={() => this.handleFilterClick("FAVES")}
+            className={`film-list-filter ${isFavesActive}`}
+            onClick={() => this.handleFilterClick(`faves`)}
           >
             FAVES
             {/* <span>{this.props.films.length}</span> */}
